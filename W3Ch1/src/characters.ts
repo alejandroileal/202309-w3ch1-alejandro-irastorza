fa2efe6729314c0058c8611c35f7e9439dbb01a4
alejandroileal <aleal1202@outlook.com>
2023-10-24 00:46:43 +0200

    Add interface and class

--- W3Ch1/src/characters.ts
+++ W3Ch1/src/characters.ts
@@ -1,55 +1,22 @@
-export class Character {
+export interface CharactersProperties {
   name: string;
   family: string;
   age: number;
   lifeStatus: boolean;
+  emoji: string;
+  dialogue?: string;
+  yearsOfLeading?: number;
+  weapon?: string;
+  skillLevel?: number;
+  charToAdvice?: string;
+  servility?: number;
+}
 
+export class CharacterMaster implements CharactersProperties {
   constructor(name: string, family: string, age: number, lifeStatus: boolean) {
     this.name = name;
     this.family = family;
     this.age = age;
     this.lifeStatus = lifeStatus;
-  }
-}
-
-export function renderCharacters(
-  characters = Array,
-  selector = HTMLAllCollection
-) {
-  for (const char of characters) {
-    selector.innerHTML += `<li class="character col">
-        <div class="card character__card">
-          <img src="./src/img/no-one.jpg" alt="Nombre y familia del personaje"
-            class="character__picture card-img-top" />
-          <div class="card-body">
-            <h2 class="character__name card-title h4">${char.name} (${char.family})</h2>
-            <div class="character__info">
-              <ul class="list-unstyled">
-                <li>Edad: ${char.age} años</li>
-                <li>
-                  Estado:
-                  <i class="fas fa-thumbs-down"></i>
-                  <i class="fas fa-thumbs-up"></i>
-                </li>
-              </ul>
-            </div>
-            <div class="character__overlay">
-              <ul class="list-unstyled">
-                <li>Años de reinado: X</li>
-                <li>Arma: XXX</li>
-                <li>Destreza: X</li>
-                <li>Peloteo: X</li>
-                <li>Asesora a: X</li>
-                <li>Sirve a: X</li>
-              </ul>
-              <div class="character__actions">
-                <button class="character__action btn">habla</button>
-                <button class="character__action btn">muere</button>
-              </div>
-            </div>
-          </div>
-          <i class="emoji"></i>
-        </div>
-      </li>`;
   }
 }

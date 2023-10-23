5e3ad8c25ea4f0fd1ea122cc0f3bc5e92df3eb79
alejandroileal <aleal1202@outlook.com>
2023-10-23 22:45:27 +0200

    Add character info

--- W3Ch1/src/main.ts
+++ W3Ch1/src/main.ts
@@ -1,24 +1,23 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import './style.css';
+import { Character, renderCharacters } from './characters';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+function main() {
+  console.log('Loaded Main :)');
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  let domCharacterListElement =
+    document.querySelector<HTMLDivElement>('.characters-list');
+
+  if (domCharacterListElement === null) return;
+
+  const characters = [
+    new Character('Joffrey Baratheon', 'rey', 50, true),
+    new Character('Jaime Lannister', 'luchador', 35, true),
+    new Character('Daenerys Targaryen', 'luchadora', 29, true),
+    new Character('Tyrion Lannister', 'asesor de Daenerys', 43, true),
+    new Character('Bronn', 'escudero de Jaime', 43, true),
+  ];
+
+  renderCharacters(characters, domCharacterListElement);
+}
+
+main();
